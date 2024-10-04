22f00bb34af82d812d7d4dfd5333d4fc7008d4e2
TIENDAT23-LAPTO\Tien Dat <nguyentiendat9904@gmail.com>
2024-10-04 17:16:24 +0700

    add embeddings to lstm

--- models/lstm/LSTM.py
+++ models/lstm/LSTM.py
@@ -13,22 +13,26 @@ from builders.model_builder import META_ARCHITECTURE
 class LSTM_Model(nn.Module):
     def __init__(self, config, vocab: Vocab):
         super(LSTM_Model, self).__init__()
+        NUMBER_OF_COMPONENTS = 3
         self.device= config.device
-        self.d_model = config.hidden_dim 
-        self.hidden_dim = config.hidden_dim 
+        self.d_model = config.d_model  * NUMBER_OF_COMPONENTS
         self.layer_dim = config.layer_dim
-
-        self.lstm = nn.LSTM(config.input_dim, self.hidden_dim, self.layer_dim, batch_first=True, dropout=config.dropout if self.layer_dim > 1 else 0)
+        self.hidden_dim = config.hidden_dim
+        self.embedding = nn.Embedding(vocab.total_tokens, config.d_model, padding_idx=0)
+        self.lstm = nn.LSTM(config.input_dim, self.d_model, self.layer_dim, batch_first=True, dropout=config.dropout if self.layer_dim > 1 else 0)
         self.dropout = nn.Dropout(config.dropout)
-        self.fc = nn.Linear(self.hidden_dim, 2)
+        self.fc = nn.Linear(self.d_model, 2)
         self.loss = nn.CrossEntropyLoss()
 
     def forward(self, x, labels):
         
+        x = self.embedding(x)
+        x = x.reshape(x.size(0), x.size(1), -1)
+        
         batch_size = x.size(0)
 
         h0, c0 = self.init_hidden(batch_size, self.device)
-        out, (hn, cn) = self.lstm(x.float(), (h0.detach(), c0.detach()))
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
 
         out = self.dropout(out[:, -1, :])
 
