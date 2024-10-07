8e81521fe77218c1382dfe080b290d706bb2ec87
TIENDAT23-LAPTO\Tien Dat <nguyentiendat9904@gmail.com>
2024-10-07 14:37:44 +0700

    edit main bugs

--- vocabs/viphervocab.py
+++ vocabs/viphervocab.py
@@ -105,7 +105,9 @@ class ViPherVocab(Vocab):
         words = []
         current_word = []
         i = 0  # Main loop index
-        encoded_sentence = encoded_sentence[0]
+      
+        encoded_sentence = encoded_sentence
+        
         while i < len(encoded_sentence):
 
             onset_idx, tone_idx, rhyme_idx = encoded_sentence[i].tolist()
