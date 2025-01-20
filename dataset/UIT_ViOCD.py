a2a5d0bf50e82fee1c488286f2f1605fce4d0d47
Minh Hieu Huynh <22520434@gm.uit.edu.vn>
2025-01-20 16:02:02 +0700

    update config gen, dataset for ViOCD

---
+++ dataset/UIT_ViOCD.py
@@ -0,0 +1,61 @@
+from torch.utils.data import Dataset
+
+from builders.dataset_builder import META_DATASET
+from vocabs.vocab import Vocab
+from utils.instance import Instance
+import pandas as pd
+import json
+
+
+@META_DATASET.register()
+class UIT_ViOCD_Dataset_Domain(Dataset):
+    def __init__(self, config, vocab: Vocab):
+        super().__init__()
+
+        path: str = config.path
+        self._data = json.load(open(path,  encoding='utf-8'))
+        self._vocab = vocab
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __getitem__(self, index: int) -> Instance:
+        item = self._data[index]
+
+        sentence = item["review"]
+        label = item["domain"]
+
+        encoded_sentence = self._vocab.encode_sentence(sentence)
+        encoded_label = self._vocab.encode_label(label)
+
+        return Instance(
+            input_ids=encoded_sentence,
+            label=encoded_label
+        )
+
+
+@META_DATASET.register()
+class UIT_ViOCD_Dataset_Label(Dataset):
+    def __init__(self, config, vocab: Vocab):
+        super().__init__()
+
+        path: str = config.path
+        self._data = json.load(open(path,  encoding='utf-8'))
+        self._vocab = vocab
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __getitem__(self, index: int) -> Instance:
+        item = self._data[index]
+
+        sentence = item["review"]
+        label = item["label"]
+
+        encoded_sentence = self._vocab.encode_sentence(sentence)
+        encoded_label = self._vocab.encode_label(label)
+
+        return Instance(
+            input_ids=encoded_sentence,
+            label=encoded_label
+        )
--- dataset/__init__.py
+++ dataset/__init__.py
@@ -3,6 +3,7 @@ from utils.instance import Instance, InstanceList
 
 from .UIT_VSFC import UIT_VSFC_Dataset_Topic, UIT_VSFC_Dataset_Sentiment
 from .UIT_ViCTSD import UIT_ViCTSD_Dataset_Toxic, UIT_ViCTSD_Dataset_Construct
+from .UIT_ViOCD import UIT_ViOCD_Dataset_Domain, UIT_ViOCD_Dataset_Label
 
 def collate_fn(items: List[Instance]) -> InstanceList:
     return InstanceList(items)
