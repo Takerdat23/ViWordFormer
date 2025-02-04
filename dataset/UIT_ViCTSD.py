c51b102763bb96ee4a992c56df46dee9e87ae6f5
Minh Hieu Huynh <22520434@gm.uit.edu.vn>
2025-02-04 22:58:03 +0700

    add gen config TextCNN

--- dataset/UIT_ViCTSD.py
+++ dataset/UIT_ViCTSD.py
@@ -44,6 +44,12 @@ class UIT_ViCTSD_Dataset_Construct(Dataset):
         super().__init__()
 
         path: str = config.path
+        
+        if config.get('max_len', None) is not None:
+            self._max_len = config.max_len
+        else:
+            self._max_len = None
+            
         self._data = json.load(open(path,  encoding='utf-8'))
         self._vocab = vocab
 
